e0e1783bd0ac22ee9a6b99e91992d02f2405f9ec
frandy820 <frandy820@users.noreply.github.com>
2026-09-05 10:26:29 +0800

    game13 数字华容道：十五拼图(3/4/5档+数字/图片双皮肤+程序化黄昏山水)；修tapCell死循环+verify模式依赖两真bug；hub十三合一+SW v1.0.12

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.11';
+const VERSION = 'v1.0.12';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -21,6 +21,7 @@ const PRECACHE = [
   './game10/index.html',
   './game11/index.html',
   './game12/index.html',
+  './game13/index.html',
 ];
 
 self.addEventListener('install', (e) => {
