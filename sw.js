c554729c4a68a7de49f6f380caee4f8ba0b321a8
frandy820 <frandy820@users.noreply.github.com>
2026-09-06 08:43:34 +0800

    game22 叠翠楼（堆塔 timing）：22合1 hub + v1.0.22

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.21';
+const VERSION = 'v1.0.22';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -30,6 +30,7 @@ const PRECACHE = [
     './game19/index.html',
     './game20/index.html',
     './game21/index.html',
+    './game22/index.html',
 ];
 
 self.addEventListener('install', (e) => {
