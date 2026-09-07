6887a3889ccbf184f10163bbe8a09dd40e7d247a
frandy820 <frandy820@users.noreply.github.com>
2026-09-07 10:26:58 +0800

    game51 市集连连看：五门禁全过（canLink穷举/findAnyLink千次有解/洗牌保证/死局免费洗牌/bot三关通关/G3像素级复审PASS+minor五条处置/hub 51卡 v1.0.53）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.52';
+const VERSION = 'v1.0.53';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -79,6 +79,8 @@ const PRECACHE = [
     './game48/index.html',
     './game50/',
     './game50/index.html',
+    './game51/',
+    './game51/index.html',
 ];
 
 self.addEventListener('install', (e) => {
