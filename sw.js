1cd8c98619ec6c5f565b664e8f64d211039aded1
frandy820 <frandy820@users.noreply.github.com>
2026-09-07 23:59:53 +0800

    v1.0.60: +game58 一杆清台(夜市球房九球,拖杆击打+幽灵球瞄准+按序落袋,八关) 五十八合一

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.59';
+const VERSION = 'v1.0.60';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -93,6 +93,8 @@ const PRECACHE = [
     './game56/index.html',
     './game57/',
     './game57/index.html',
+    './game58/',
+    './game58/index.html',
 ];
 
 self.addEventListener('install', (e) => {
