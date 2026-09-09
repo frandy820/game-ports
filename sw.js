0878a01b25d23d0353ca881c46e2816bfb3b2b25
frandy820 <frandy820@users.noreply.github.com>
2026-09-09 12:29:03 +0800

    game79 诗序复原：名句字序复原(60句=25/20/15,位移线+两两非重排+构造期120次预检)；hub 79卡+sw v1.0.81

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.80';
+const VERSION = 'v1.0.81';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -116,6 +116,7 @@ const PRECACHE = [
   './game76/index.html',
   './game77/index.html',
   './game78/index.html',
+  './game79/index.html',
 ];
 
 self.addEventListener('install', (e) => {
