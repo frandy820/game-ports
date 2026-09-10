3a1d5dfb220a5af89ddab022df37f6b5623aa3db
frandy820 <frandy820@users.noreply.github.com>
2026-09-11 03:37:25 +0800

    game94 糖画师傅：轨迹临摹糖画+温糖乘子+出摊图鉴；hub v1.0.96 (94 games)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.95';
+const VERSION = 'v1.0.96';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -131,6 +131,7 @@ const PRECACHE = [
   './game91/index.html',
   './game92/index.html',
   './game93/index.html',
+  './game94/index.html',
 ];
 
 self.addEventListener('install', (e) => {
