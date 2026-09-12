7e89d81d5cb225c5fc3f24a7c505a787550163e0
frandy820 <frandy820@users.noreply.github.com>
2026-09-12 13:48:42 +0800

    g104 雾港灯哨上线：海战棋机制夜雾渔港探照意象（双盲布阵+追击推理+击沉排除），hub 104 卡+title/kicker/tagline 三件套同步 v1.0.106

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.105';
+const VERSION = 'v1.0.106';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -141,6 +141,7 @@ const PRECACHE = [
   './game101/index.html',
   './game102/index.html',
   './game103/index.html',
+  './game104/index.html',
 ];
 
 self.addEventListener('install', (e) => {
