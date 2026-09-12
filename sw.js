215ed7d8c15eb738cb000b069f636d63742035e0
frandy820 <frandy820@users.noreply.github.com>
2026-09-13 00:36:08 +0800

    g110 盘灶：Tetris 下落拼行（农家盘土灶），7-bag+四态踢墙+锁定延迟0.4s+触屏三分区，审查两轮至PASS（锁延语义/13minor），hub 110 款 v1.0.112

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.111';
+const VERSION = 'v1.0.112';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -147,6 +147,7 @@ const PRECACHE = [
   './game107/index.html',
   './game108/index.html',
   './game109/index.html',
+  './game110/index.html',
 ];
 
 self.addEventListener('install', (e) => {
