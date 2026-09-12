22906144ba2456faa6d7216b43a3e27f9191e782
frandy820 <frandy820@users.noreply.github.com>
2026-09-12 10:21:19 +0800

    g101 转碟艺人上线：夜市五竿手眼六路（垂危红闪急救+波末补满制），hub 101 卡+title/kicker/tagline 三件套同步 v1.0.103

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.102';
+const VERSION = 'v1.0.103';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -138,6 +138,7 @@ const PRECACHE = [
   './game98/index.html',
   './game99/index.html',
   './game100/index.html',
+  './game101/index.html',
 ];
 
 self.addEventListener('install', (e) => {
