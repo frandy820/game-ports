4739eca7e956ac700593a11a9302cd46877222da
frandy820 <frandy820@users.noreply.github.com>
2026-09-15 01:45:32 +0800

    v1.0.128: add game126 璎珞转珠 (126 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.127';
+const VERSION = 'v1.0.128';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -163,6 +163,7 @@ const PRECACHE = [
     './game123/index.html',
     './game124/index.html',
     './game125/index.html',
+    './game126/index.html',
 ];
 
 self.addEventListener('install', (e) => {
