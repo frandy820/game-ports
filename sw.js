347ec5f292365863e9f6f51cd495a436a3617910
frandy820 <frandy820@users.noreply.github.com>
2026-09-14 20:53:39 +0800

    v1.0.126: add game124 鞠门争筹 (124 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.125';
+const VERSION = 'v1.0.126';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -161,6 +161,7 @@ const PRECACHE = [
     './game121/index.html',
     './game122/index.html',
     './game123/index.html',
+    './game124/index.html',
 ];
 
 self.addEventListener('install', (e) => {
