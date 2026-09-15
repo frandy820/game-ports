54226ddcc398fe7966be3fd10267f425811613cb
frandy820 <frandy820@users.noreply.github.com>
2026-09-16 00:28:58 +0800

    v1.0.130: add game128 春圃播粟 (128 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.129';
+const VERSION = 'v1.0.130';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -165,6 +165,7 @@ const PRECACHE = [
     './game125/index.html',
     './game126/index.html',
     './game127/index.html',
+  './game128/index.html',
 ];
 
 self.addEventListener('install', (e) => {
