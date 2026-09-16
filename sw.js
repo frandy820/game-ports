4404ba976cab7ca173a180a47f196ff0e61c5809
frandy820 <frandy820@users.noreply.github.com>
2026-09-16 12:25:04 +0800

    v1.0.131: add game129 锦杼挑纹 (129 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.130';
+const VERSION = 'v1.0.131';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -166,6 +166,7 @@ const PRECACHE = [
     './game126/index.html',
     './game127/index.html',
   './game128/index.html',
+  './game129/index.html',
 ];
 
 self.addEventListener('install', (e) => {
