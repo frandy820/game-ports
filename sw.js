c78fee0905365b3f3a9d09914ee9309da872cbdb
frandy820 <frandy820@users.noreply.github.com>
2026-09-18 00:54:53 +0800

    v1.0.138: add game135 打瓦 (135 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.137';
+const VERSION = 'v1.0.138';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -172,6 +172,7 @@ const PRECACHE = [
   './game132/index.html',
   './game133/index.html',
     './game134/index.html',
+    './game135/index.html',
 ];
 
 self.addEventListener('install', (e) => {
