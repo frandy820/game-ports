811b48cecfe8699fd42ab5f4e8e6472d7e76bdb1
frandy820 <frandy820@users.noreply.github.com>
2026-09-17 10:39:50 +0800

    v1.0.135: add game132 霹雳炮车 (132 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.134';
+const VERSION = 'v1.0.135';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -169,6 +169,7 @@ const PRECACHE = [
   './game129/index.html',
   './game130/index.html',
   './game131/index.html',
+  './game132/index.html',
 ];
 
 self.addEventListener('install', (e) => {
