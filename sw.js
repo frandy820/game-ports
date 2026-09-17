2b5e87052ac3edad0da0228548ac801a3bdd52a3
frandy820 <frandy820@users.noreply.github.com>
2026-09-17 12:14:45 +0800

    v1.0.136: add game133 翻印 (133 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.135';
+const VERSION = 'v1.0.136';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -170,6 +170,7 @@ const PRECACHE = [
   './game130/index.html',
   './game131/index.html',
   './game132/index.html',
+  './game133/index.html',
 ];
 
 self.addEventListener('install', (e) => {
