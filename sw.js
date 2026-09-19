87ee57e259b3c41526b04948dc14546c90927eca
frandy820 <frandy820@users.noreply.github.com>
2026-09-20 02:10:16 +0800

    v1.0.141: add game138 叠匣 (138 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.140';
+const VERSION = 'v1.0.141';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -175,6 +175,7 @@ const PRECACHE = [
     './game135/index.html',
     './game136/index.html',
     './game137/index.html',
+    './game138/index.html',
 ];
 
 self.addEventListener('install', (e) => {
