79822f6e9e69dd2997501618db16f6b8d11ce1c0
frandy820 <frandyworkstation@gmail.com>
2026-09-20 13:46:09 +0800

    v1.0.145: add game141 震天雷 校射砲战 (141 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.144';
+const VERSION = 'v1.0.145';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -178,6 +178,7 @@ const PRECACHE = [
     './game138/index.html',
     './game139/index.html',
     './game140/index.html',
+    './game141/index.html',
 ];
 
 self.addEventListener('install', (e) => {
