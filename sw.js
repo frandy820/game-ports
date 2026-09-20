0bfc69f8efd17a87e9f3b6d7f03b7ef22e5b3180
frandy820 <frandyworkstation@gmail.com>
2026-09-20 13:49:17 +0800

    v1.0.147: add game143 独钓寒江 钓鱼时机 (143 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.146';
+const VERSION = 'v1.0.147';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -180,6 +180,7 @@ const PRECACHE = [
     './game140/index.html',
     './game141/index.html',
     './game142/index.html',
+    './game143/index.html',
 ];
 
 self.addEventListener('install', (e) => {
