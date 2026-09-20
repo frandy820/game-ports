8eb066084360746629328724d09e8c2636a2ad8e
frandy820 <frandyworkstation@gmail.com>
2026-09-20 16:29:51 +0800

    g146 雁塔归经（汉诺塔族三型变体 CL/ADJ/BI）：146 GAMES, sw v1.0.151

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.150';
+const VERSION = 'v1.0.151';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -183,6 +183,7 @@ const PRECACHE = [
     './game143/index.html',
     './game144/index.html',
     './game145/index.html',
+    './game146/index.html',
 ];
 
 self.addEventListener('install', (e) => {
