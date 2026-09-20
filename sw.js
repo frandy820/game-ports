4381233226b1a7fa6e4409adcdf4cd0fdf1d157f
frandy820 <frandyworkstation@gmail.com>
2026-09-20 18:55:29 +0800

    g148 汴河编纲 v1.0.153 (148 GAMES)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 口袋游戏机 ServiceWorker · stale-while-revalidate：离线立开，改动后台更新 */
-const VERSION = 'v1.0.152';
+const VERSION = 'v1.0.153';
 const CACHE = 'pocket-games-' + VERSION;
 const PRECACHE = [
   './',
@@ -185,6 +185,7 @@ const PRECACHE = [
     './game145/index.html',
     './game146/index.html',
     './game147/index.html',
+    './game148/index.html',
 ];
 
 self.addEventListener('install', (e) => {
